099033abcdd3c592d3c36af32c7edad224a739a4
Jung seo young <sju06071@gmail.com>
2022-09-30 19:20:40 +0900

    remove posts action...

--- board_app/src/store/actions/actions.ts
+++ board_app/src/store/actions/actions.ts
@@ -2,11 +2,13 @@
 import { createAction } from "typesafe-actions";
 import { postItem } from "../types/types";
 export const DELETE_POST_ITEM = "post/DELETE_POST_ITEM";;
+export const DELETE_POST_ITEMS = 'post/DELETE_POST_ITEMS'
 export const ADD_POST_ITEM = "post/ADD_POST_ITEM";
 export const UPDATE_POST_ITEM = "post/UPDATE_POST_ITEM";;
 export const UPDATE_ID = 'post/UPDATE_ID'
 // 액션함수 선언
 export const deletePost = createAction(DELETE_POST_ITEM)<{index: number}>();
+export const deletePosts = createAction(DELETE_POST_ITEMS)<{removePostsList: number[]}>();
 export const addPost = createAction(ADD_POST_ITEM)<{post: postItem}>()
 export const updateID = createAction(UPDATE_ID)<{}>();
 export const updatePost = createAction(UPDATE_POST_ITEM)<{index : number, newPost: postItem}>()
--- board_app/src/store/reducer/reducer.ts
+++ board_app/src/store/reducer/reducer.ts
@@ -3,8 +3,8 @@ import {
     , ActionType
     , createReducer
 } from 'typesafe-actions';
-import {ADD_POST_ITEM, DELETE_POST_ITEM, UPDATE_ID, UPDATE_POST_ITEM} from '../actions/actions';
-import { postActions, postStoreType } from '../types/types';
+import {ADD_POST_ITEM, DELETE_POST_ITEM, DELETE_POST_ITEMS, UPDATE_ID, UPDATE_POST_ITEM} from '../actions/actions';
+import {postActions, postItem, postStoreType} from '../types/types';
 import {compose, createStore, PreloadedState, Store} from "redux";
 import rootReducer, {RootState} from "../index";
 
@@ -15,7 +15,17 @@ declare global {
         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
     }
 }
-
+const deletePosts = (removeList: number[], postList: postItem[]) =>{
+    postList.map((item)=>{
+        if(removeList.includes(item.id)){
+            let i = postList.indexOf(item)
+            postList.splice(i,1)
+        }
+    })
+    console.log('remove',postList)
+    return postList
+}
+//state.todo.filter(item => item.id !== action.payload.id)
 // 리듀서 추가
 const postReducer = createReducer<postStoreType, postActions>(initialState, {
     [ADD_POST_ITEM]: (state, action) =>({
@@ -26,6 +36,10 @@ const postReducer = createReducer<postStoreType, postActions>(initialState, {
         ...state,
         postList : [...state.postList.slice(0,action.payload.index),...state.postList.slice(action.payload.index+1)]
     }),
+    [DELETE_POST_ITEMS]: (state, action) =>({
+        ...state,
+        postList : deletePosts(action.payload.removePostsList, state.postList)
+    }),
     [UPDATE_POST_ITEM]: (state, action) =>({
         ...state,
         postList : [...state.postList.slice(0,action.payload.index),action.payload.newPost, ...state.postList.slice(action.payload.index+1)]
@@ -34,7 +48,6 @@ const postReducer = createReducer<postStoreType, postActions>(initialState, {
         ...state,
         writeID: state.writeID+1
     })
-
 })
 
 export default postReducer;
